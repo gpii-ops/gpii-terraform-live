30e1266a3cecbbe7ce4a7d48591c28de5680580a
Tyler Roscoe <tyler@cryptio.net>
2017-04-25 23:45:44 -0600

    WIP: terragrunt_wrapper

--- dev/main.tf
+++ dev/main.tf
@@ -1,1 +1,6 @@
-# Empty file to prevent terragrunt apply-all from freaking out.
+# Mostly-empty file to prevent 'terragrunt apply-all' from freaking out.
+
+# Fake output to prevent 'terraform output' from freaking out.
+output "variable_so_terraform_output_doesnt_freak_out" {
+  value = "yuuuuuup"
+}
