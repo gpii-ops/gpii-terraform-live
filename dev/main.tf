8d18345639ca8f396de1e41a19bb634ee68edd3e
Tyler Roscoe <tyler@cryptio.net>
2017-05-05 18:06:23 -0600

    This placeholder output is no longer needed.

--- dev/main.tf
+++ dev/main.tf
@@ -1,6 +1,1 @@
 # Mostly-empty file to prevent 'terragrunt apply-all' from freaking out.
-
-# Fake output to prevent 'terraform output' from freaking out.
-output "variable_so_terraform_output_doesnt_freak_out" {
-  value = "yuuuuuup"
-}
